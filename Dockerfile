008ccc7f28a93f4ea0112127e92014127bdc0f0b
Samuel Loza <samuel.loza26@gmail.com>
2024-04-15 19:56:38 -0400

    Add ci/cd

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+
+COPY . .
+
+WORKDIR "/src/"
+
+RUN dotnet restore "src/Presentation/OnlineJudgeAdminApi/OnlineJudgeAdminApi.csproj"
+
+
+RUN dotnet build "src/Presentation/OnlineJudgeAdminApi/OnlineJudgeAdminApi.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "src/Presentation/OnlineJudgeAdminApi/OnlineJudgeAdminApi.csproj" -c Release -o /app/publish
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS final
+WORKDIR /app
+
+COPY --from=publish /app/publish .
+
+ENTRYPOINT ["dotnet", "OnlineJudgeAdminApi.csproj"]
--- src/Infrastructure/OnlineJudgeAdmin.Infrastructure.AwsS3/Implementations/AwsS3Manager.cs
+++ src/Infrastructure/OnlineJudgeAdmin.Infrastructure.AwsS3/Implementations/AwsS3Manager.cs
@@ -1,21 +1,50 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.Extensions.Configuration;
 using OnlineJudgeAdmin.Core.Domain.Abstractions.Infrastructure;
 
 namespace OnlineJudgeAdmin.Infrastructure.AwsS3.Implementations;
-public class AwsS3Manager: IAwsS3FileManager
+public class AwsS3Manager : IAwsS3FileManager
 {
+    private readonly AmazonS3Client _s3Client;
     private readonly IConfiguration _configuration;
     private readonly string _path;
 
     public AwsS3Manager(IConfiguration configuration)
     {
         _configuration = configuration;
+        var credentials = new BasicAWSCredentials("***REMOVED_AWS_ACCESS_KEY***", "***REMOVED_AWS_SECRET_KEY***");
+        _s3Client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
         _path = _configuration["FileSettings:ProblemsFilePath"];
     }
 
-    public async Task<string> UploadFileAsync(string bucketName, string keyName, string filePath)
+    public async Task<string> S3UploadFileAsync(string bucketName, string keyName, string filePath)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var fileTransferUtility = new TransferUtility(_s3Client);
+
+            var uploadRequest = new TransferUtilityUploadRequest
+            {
+                BucketName = bucketName,
+                Key = keyName,
+                FilePath = filePath,
+                CannedACL = S3CannedACL.PublicRead
+            };
+
+            await fileTransferUtility.UploadAsync(uploadRequest);
+            return $"https://{bucketName}.s3.{_s3Client.Config.RegionEndpoint.SystemName}.amazonaws.com/{keyName}";
+        }
+        catch (AmazonS3Exception e)
+        {
+            Console.WriteLine($"Error encountered on server. Message:'{e.Message}' when writing an object");
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unknown encountered on server. Message:'{e.Message}'");
+            throw;
+        }
     }
 }
-
