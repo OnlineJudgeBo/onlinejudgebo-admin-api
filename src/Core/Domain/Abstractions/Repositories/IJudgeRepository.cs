6d1d1af5c6833491a84f9b59b7e42555c7713dbf
Samuel Loza <samuel.loza26@gmail.com>
2024-05-08 22:18:09 -0400

    Add Rejudge

---
+++ src/Core/Domain/Abstractions/Repositories/IJudgeRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineJudgeAdmin.Core.Domain.Abstractions.Repositories;
+
+public interface IJudgeRepository
+{
+    public Task RejudgeSolutionByIdAsync(int solutionId);
+    public Task RejudgeSolutionByProblemIdAsync(int problemId);
+}
---
+++ src/Core/Domain/Abstractions/Services/IJudgeService.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineJudgeAdmin.Core.Domain.Abstractions.Services;
+
+public interface IJudgeService
+{
+    public Task RejudgeSolutionByIdAsync(int solutionId);
+    public Task RejudgeSolutionByProblemIdAsync(int problemId);
+}
