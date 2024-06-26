4d31ead0117661e9f3776b331a22a11c215034ac
Samuel Loza <samuel.loza26@gmail.com>
2024-06-25 20:30:20 -0400

    Add topic service

--- src/Core/Application/Services/Implementations/TopicService.cs
+++ src/Core/Application/Services/Implementations/TopicService.cs
@@ -1,4 +1,4 @@
-﻿using FluentValidation;
+using FluentValidation;
 
 using OnlineJudgeAdmin.Core.Domain.Abstractions.Repositories;
 using OnlineJudgeAdmin.Core.Domain.Abstractions.Services;
@@ -22,6 +22,33 @@ public class TopicService : ITopicService
     public async Task<IEnumerable<Topic>> GetAllTopicsAsync()
     {
         return await _topicRepository.GetAllTopicsAsync();
+    }
+
+    public async Task AddTopicAsync(Topic newTopic)
+    {
+        await _topicRepository.CreateTopic(newTopic);
+    }
+
+    public async Task AddClassificationToTopic(Topic topic)
+    {
+        if (topic.Classifications == null || !topic.Classifications.Any())
+        {
+            throw new ArgumentException("No classifications provided.");
+        }
+
+        await _topicRepository.AddClassificationToTopic(topic);
+    }
+
+    public async Task UpdateClassification(Classification classification)
+    {
+        var existingClassification = await _topicRepository.GetClassificationById(classification.ClassificationId);
+
+        if (existingClassification == null)
+        {
+            throw new ArgumentException("Classification not found.");
+        }
+
+        await _topicRepository.UpdateClassification(classification);
     }
 }
 
--- src/Core/Domain/Abstractions/Services/ITopicService.cs
+++ src/Core/Domain/Abstractions/Services/ITopicService.cs
@@ -1,9 +1,12 @@
-﻿using OnlineJudgeAdmin.Core.Domain.Models;
+using OnlineJudgeAdmin.Core.Domain.Models;
 
 namespace OnlineJudgeAdmin.Core.Domain.Abstractions.Services;
 
 public interface ITopicService
 {
-    public Task<IEnumerable<Topic>> GetAllTopicsAsync();
+    Task<IEnumerable<Topic>> GetAllTopicsAsync();
+    Task AddTopicAsync(Topic newTopic);
+    Task AddClassificationToTopic(Topic newTopic);
+    Task UpdateClassification(Classification classification);
 }
 
